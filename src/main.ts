cb154e29633ac09afaf3c3356361f95c977e4d92
shinka-dev <thomas.shinkarenko@gmail.com>
2021-11-01 20:20:37 -0300

    Cors added - integration with front-end in progress

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   await app.listen(4000);
   logger.log(`Application listening on port 4000`);
 }

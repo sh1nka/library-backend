af388b527bb069916ac8ce0204155bf566368883
shinka-dev <thomas.shinkarenko@gmail.com>
2021-11-01 20:10:21 -0300

    config schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
     }),
     BooksModule,
     TypeOrmModule.forRootAsync({
